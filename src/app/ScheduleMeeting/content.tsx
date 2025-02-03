3a439ea76e34f78bcca567f1c65a1b6da308f6b0
M-Ehsan-Ali <mrehsan51@gmail.com>
2025-02-03 19:43:20 +0500

    meeting responsive deign done

--- src/app/ScheduleMeeting/content.tsx
+++ src/app/ScheduleMeeting/content.tsx
@@ -1,17 +1,24 @@
 export default function ScheduleMeeting() {
   return (
-    <div id="ScheduleMeeting" className="mb-[190px] mt-[148px]">
+    <div
+      id="ScheduleMeeting"
+      className="mb-[0px] sm:mb-[190px] mt-[50px] sm:mt-[148px]"
+    >
       {/* Schedule Meeting Section */}
-      <div className="flex flex-col items-center px-[182px] text-center">
-        <p className="font-outfit font-bold text-white text-[54px]">
+      <div className="flex flex-col items-center px-[30px] sm:px-[182px] text-center">
+        <p className="font-outfit font-bold text-white text-[14px] sm:text-[54px]">
           Schedule a Meeting with Holborn Relocate
         </p>
-        <p className="font-alata font-normal text-white text-[32px] mt-[24px]">
+        <p className="font-alata font-normal text-white text-[12px] sm:text-[32px] mt-[24px]">
           Connect with our expert team to discuss your smooth transition to
           Dubai and explore the services that make relocating easy and
           stress-free.
         </p>
-        <img src="/scheduleMeet.png" alt="schedule" className="mt-[90px]" />
+        <img
+          src="/scheduleMeet.png"
+          alt="schedule"
+          className="mt-[24px] sm:mt-[90px]"
+        />
       </div>
       {/* Gateway Section */}
     </div>
